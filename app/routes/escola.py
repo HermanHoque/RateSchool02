5152330d443130d1432a04a93b207ded4e73734f
HermanHoque <hermanhoque13@gmail.com>
2025-11-03 20:43:04 +0100

    pagina de login

--- app/routes/escola.py
+++ app/routes/escola.py
@@ -6,5 +6,10 @@ escola_bp = Blueprint('escola', __name__)
 @escola_bp.route('/criarConta')
 def criarConta():
 
-    return render_template('criarConta.html', title='Criar Conta')
+    return render_template('escola/criarConta.html', title='Criar Conta')
 
+
+@escola_bp.route('/loginEscola')
+def login():
+
+    return render_template('escola/login.html', title='Login Escola')
