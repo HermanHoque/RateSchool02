dca1044b0892b744b5f540b7054d47b1b386053c
HermanHoque <hermanhoque13@gmail.com>
2025-12-24 00:47:26 +0100

    rota admin

--- app/routes/index.py
+++ app/routes/index.py
@@ -6,3 +6,7 @@ index_bp = Blueprint('index', __name__)
 @index_bp.route('/')
 def index():
     return render_template('index.html', title='Home')
+
+@index_bp.route('/sobre-nos')
+def sobreNos():
+    return render_template('sobreNos.html', title='Sobre Nós')
